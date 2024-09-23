9eed2771d4ef1fe13f9f0064a4cc87b9832fec97
codewithmokone <codewithmokone@gmail.com>
2024-09-23 14:16:06 +0200

    added logic for the sort functionality in the collection page

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -11,6 +11,7 @@ const Collection = () => {
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
   const [subCategory, setSubCategory] = useState([]);
+  const [sortType,setSortType] = useState('relavent')
 
   // Function for category filter
   const toggleCategory = (e) => {
@@ -45,9 +46,32 @@ const Collection = () => {
     setFilterProducts(productsCopy);
   }
 
+
+  const sortProduct = () => {
+    let fpCopy = filterProducts.slice();
+
+    switch (sortType) {
+      case 'low-high':
+        setFilterProducts(fpCopy.sort((a,b)=>(a.price - b.price)));
+        break;
+
+      case 'high-low':
+        setFilterProducts(fpCopy.sort((a,b)=>(b.price - a.price)));
+        break;
+
+      default:
+        applyFilter();
+        break
+    }
+  }
+
   useEffect(() => {
     applyFilter();
   },[category,subCategory])
+
+  useEffect(()=>{
+    sortProduct()
+  },[sortType]);
 
 
   return (
@@ -130,7 +154,7 @@ const Collection = () => {
         <div className="flex justify-between text-base sm:text-2xl mb-4">
           <Title text1={"ALL"} text2={"COLLECTIONS"} />
           {/* Product sort */}
-          <select className="border border-gray-300 text-sm px-2">
+          <select onChange={(e)=>setSortType(e.target.value)} className="border border-gray-300 text-sm px-2">
             <option value="relavent">Sort by: Relavent</option>
             <option value="low-high">Sort by: Low to High</option>
             <option value="high-low">Sort by: High to Low</option>
