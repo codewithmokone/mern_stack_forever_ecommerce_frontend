5eda19ad59a8459b9d1d70b06b60d49d10d696c4
codewithmokone <codewithmokone@gmail.com>
2024-09-23 10:02:18 +0200

    added a collections page with the filter functionality

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const NewsLetter = () => {
 
+    // Function for the subscribtion email 
     const onSubmitHandler = (event) => {
         event.preventDefault();
     }
